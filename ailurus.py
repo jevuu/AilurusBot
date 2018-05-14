edeeef49fc2d562c3f5474ef79c865061430e2a8
Justin Vuu <35980711+jevuu@users.noreply.github.com>
2018-05-14 02:10:19 -0400

    Added commands, background task

--- ailurus.py
+++ ailurus.py
@@ -7,11 +7,44 @@ from discord.ext.commands import Bot
 from random import randint
 import json
 import io
+import os
 import asyncio
+
+class pilot:
+
+    def __init__(self, uid:str, m:int = 10000, aP:int = 0, sF:int = 0, xp:int = 0):
+        self.userID:str = uid
+        self.money:int = m
+        self.activePlane:int = aP
+        self.sortiesFlown:int = sF
+        self.experience:int = xp
+    
+    def modMoney(self, m:int):
+        self.money += m
+
 
 Client = discord.Client()
 bot = commands.Bot(command_prefix = '.')
+with open("Config/config.json") as loadConfig:
+    config = json.load(loadConfig)
+iSPilotsPath = "InfSkies/Pilots/"
+iSDataPath = "InfSkies/Data/"
 pilots = []
+for filename in os.listdir(iSPilotsPath):
+    with open(iSPilotsPath + filename) as loadPilot:
+        lp = json.load(loadPilot)
+    p = pilot(lp["userID"],lp["money"],lp["activePlane"],lp["sortiesFlown"])
+    pilots.append(p)
+
+async def bg_task_savePilots():
+    await bot.wait_until_ready()
+    while not bot.is_closed:
+        await asyncio.sleep(5) # task runs every 5 seconds
+        for p in pilots:
+            filename:str = p.userID + ".json"
+            with open(iSPilotsPath + filename, 'w') as output:
+                json.dump(p.__dict__, output)
+        print("Saved all pilot data!")
 
 @bot.event
 async def on_ready():
@@ -73,6 +106,8 @@ async def pick(ctx, *choices:str):
 
 #GAME STUFF
 
+##Player data modification
+
 @bot.command(pass_context = True)
 async def playInf(ctx):
     title:str = "Infinite Skies registration for "
@@ -92,33 +127,68 @@ async def playInf(ctx):
         newPilot = pilot(ctx.message.author.id)
         pilots.append(newPilot)
         fileName:str = ctx.message.author.id + ".json"
-        with open(fileName, 'w') as output:
+        with open(iSPilotsPath + fileName, 'w') as output:
             json.dump(newPilot.__dict__, output)
         embed = discord.Embed(title = title + name, description = "Success! Welcome to the skies!")
     else:
         embed = discord.Embed(title = title + name, description = "You're already a pilot!")
-    
     await bot.say(embed = embed)
 
 @bot.command(pass_context = True)
-async def modMoney(ctx, b:int):
-    p = pilot("test")
-    p.modMoney(b)
-
-    await bot.say(p.money)
+async def modMoney(ctx, m:int):
+    for p in pilots:
+        if p.userID == ctx.message.author.id:
+            pI = pilots.index(p)
+            break
     
-class pilot:
+    pilots[pI].money += m
 
-    def __init__(self, uid:str):
-        self.userID:str = uid
-        self.money:int = 10000
-        self.activePlane:int = 0
-        self.sortiesFlown:int = 0
-    
-    def modMoney(self, m:int):
-        self.money += m
+    title:str = "Modifying money for "
+    name:str
+    if ctx.message.author.nick == None:
+        name = ctx.message.author.name
+    else:
+        name = ctx.message.author.nick
+    embed = discord.Embed(title = title + name, description = pilots[pI].money)
+    await bot.say(embed = embed)
 
-with open("config.json", 'r') as loadConfig:
-    config = json.loads(loadConfig)
+##Player data retrieval
 
-bot.run(config['key'])
+@bot.command(pass_context = True)
+async def pilotInfo(ctx):
+    members = ctx.message.mentions
+    if len(members) == 1:
+        title:str = "Pilot info for "
+        name:str
+        if members[0].nick == None:
+            name = members[0].name
+        else:
+            name = members[0].nick
+        
+        pilotFound:bool = False
+        for p in pilots:
+            if p.userID == members[0].id:
+                pI = pilots.index(p)
+                pilotFound = True
+                break
+
+        if pilotFound == True:
+            embed = discord.Embed(title = title + name)
+            embed.set_thumbnail(url = members[0].avatar_url)
+            embed.add_field(name = "Aircraft", value = pilots[pI].activePlane)
+            embed.add_field(name = "Sorties", value = pilots[pI].sortiesFlown, inline = True)
+            embed.add_field(name = "Credits", value = pilots[pI].money)
+            embed.add_field(name = "Experience", value = pilots[pI].experience, inline = True)
+        else:
+            embed = discord.Embed(title = title + name, description = "They are not an active pilot!")
+
+    elif len(members) > 1:
+        embed = discord.Embed(title = "Pilot Lookup Error", description = "I can only search one pilot.")
+    else:
+        embed = discord.Embed(title = "Pilot Lookup Error", description = "You need to mention an active pilot.")
+
+    #member = discord.Server.get_member(uid)
+    await bot.say(embed = embed)
+
+bot.loop.create_task(bg_task_savePilots())
+bot.run(config["token"])
